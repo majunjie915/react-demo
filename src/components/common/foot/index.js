346a3bd31dc548780bd3469210ef6574d340dd25
majunjie <majunjie@autohome.com.cn>
2020-06-19 10:09:54 +0800

    feat: 设置类名

--- src/components/common/foot/index.js
+++ src/components/common/foot/index.js
@@ -7,10 +7,10 @@ class Foot extends React.Component {
     let curIndex = this.props.curIndex;
     return (
       <ul className="foot-items">
-        <li className={curIndex === 'recomment' && 'active'}><Link to="/">推荐页</Link></li>
-        <li className={curIndex === 'home' && 'active'}><Link to="/home">首页</Link></li>
-        <li className={curIndex === 'activity' && 'active'}><Link to="/activity">活动页</Link></li>
-        <li className={curIndex === 'mine' && 'active'}><Link to="/mine">我的</Link></li>
+        <li className={curIndex === 'recomment' ? 'active' : ''}><Link to="/">推荐页</Link></li>
+        <li className={curIndex === 'home' ? 'active' : ''}><Link to="/home">首页</Link></li>
+        <li className={curIndex === 'activity' ? 'active' : ''}><Link to="/activity">活动页</Link></li>
+        <li className={curIndex === 'mine' ? 'active' : ''}><Link to="/mine">我的</Link></li>
       </ul>
     );
   }
